53d1d5001a5e8140ddf81530a05059da92c4dbe4
linyuelin <linyuelin1997@gmail.com>
2024-01-31 13:34:40 +0900

    基本仕組み

---
+++ Messenger/Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by dreaMTank on 2024/01/31.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
